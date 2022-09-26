1f37f6c3477c952e2b75060eff9ddd2d3d0b2b82
gbby <gabby.resch@utoronto.ca>
2022-09-26 11:50:02 -0400

    updates to javascript and content

--- js/index.js
+++ js/index.js
@@ -403,7 +403,7 @@ d3.csv('./data/Experience-Graph3-TreeMap.csv').then(function(data) {
         .size([width_G3treemap, height_G3treemap])
         .padding(4)
         (root);
-    
+
     // use this information to add rectangles:
     svg_G3treemap
         .selectAll("rect")
@@ -422,12 +422,12 @@ d3.csv('./data/Experience-Graph3-TreeMap.csv').then(function(data) {
             return d.y1 - d.y0;
         })
         .style("stroke", "black")
-    // .style("fill", function(d) { return colorScale(d.value)});
-	// .style("fill", function(d) { return color(d.data.parent)});    
+        // .style("fill", function(d) { return colorScale(d.value)});
+        // .style("fill", function(d) { return color(d.data.parent)});    
         .style("fill", "#0077c8");
 
-// d3.scaleOrdinal([`#002d72`, `#0077c8`, `#5bc2f4`])
-    
+    // d3.scaleOrdinal([`#002d72`, `#0077c8`, `#5bc2f4`])
+
     // and to add the text labels
     svg_G3treemap
         .selectAll("text")
@@ -445,15 +445,15 @@ d3.csv('./data/Experience-Graph3-TreeMap.csv').then(function(data) {
         .attr("font-size", "20px")
         .attr("fill", "white")
 
-//     // Adding tooltip for info on hover
-// const tooltip = d3.select('svg#canvas')
-//                   .append('div')
-//                   .attr('id', 'tooltip')
-//                   .attr('width', 60 + 'px')
-//                   .attr('height', 40 + 'px')
-//                   .style('z-index', 10)
-//                   .style('display', 'none')
-//                   .style('position', 'absolute');
+    //     // Adding tooltip for info on hover
+    // const tooltip = d3.select('svg#canvas')
+    //                   .append('div')
+    //                   .attr('id', 'tooltip')
+    //                   .attr('width', 60 + 'px')
+    //                   .attr('height', 40 + 'px')
+    //                   .style('z-index', 10)
+    //                   .style('display', 'none')
+    //                   .style('position', 'absolute');
 })
 
 
@@ -848,3 +848,175 @@ d3.csv("./data/JobMatch-Graph4.csv").then(function(data) {
         })
 
 })
+
+
+// ----------------
+// GRAPH 3 - Bubble
+// ----------------
+
+// set the dimensions and margins of the graph
+const width = 900
+const height = 500
+
+// append the svg object to the body of the page
+const svg = d3.select("#bubblegraph")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+
+// Read data
+d3.csv("./data/bubble.csv").then(function(data) {
+
+    // Filter a bit the data -> more than 1 million inhabitants
+    data = data.filter(function(d) {
+        return d.value > 0
+    })
+
+    // Color palette
+    const color = d3.scaleOrdinal()
+        .domain(["one", "two", "three", "four", "five", "six"])
+        .range(d3.schemeSet1);
+
+    // Size scale
+    const size = d3.scaleLinear()
+        .domain([0, 170])
+        .range([20, 100]) // circle will be between 7 and 55 px wide
+
+    // create a tooltip
+    const Tooltip = d3.select("#bubblegraph")
+        .append("div")
+        .style("opacity", 0)
+        .attr("class", "tooltip")
+        .style("background-color", "white")
+        .style("border", "solid")
+        .style("border-width", "2px")
+        .style("border-radius", "5px")
+        .style("padding", "5px")
+
+    // Three function that change the tooltip when user hover / move / leave a cell
+    const mouseover = function(event, d) {
+        Tooltip
+            .style("opacity", 1)
+    }
+    const mousemove = function(event, d) {
+        Tooltip
+            .html(d.value + " people" + d.key)
+            .style("left", (event.x / 2 + 20) + "px")
+            .style("top", (event.y / 2 - 30) + "px")
+    }
+    var mouseleave = function(event, d) {
+        Tooltip
+            .style("opacity", 0)
+    }
+
+    // Initialize the circle: all located at the center of the svg area
+    var node = svg.append("g")
+        .selectAll("circle")
+        .data(data)
+        .join("circle")
+        .attr("class", "node")
+        .attr("r", d => size(d.value))
+        .attr("cx", width / 2)
+        .attr("cy", height / 2)
+        .style("fill", d => color(d.region))
+        .style("fill-opacity", 0.8)
+        .attr("stroke", "black")
+        .style("stroke-width", 1)
+        .on("mouseover", mouseover) // What to do when hovered
+        .on("mousemove", mousemove)
+        .on("mouseleave", mouseleave)
+        .call(d3.drag() // call specific function when circle is dragged
+            .on("start", dragstarted)
+            .on("drag", dragged)
+            .on("end", dragended));
+
+    // Features of the forces applied to the nodes:
+    const simulation = d3.forceSimulation()
+        .force("center", d3.forceCenter().x(width / 2).y(height / 2)) // Attraction to the center of the svg area
+        .force("charge", d3.forceManyBody().strength(.1)) // Nodes are attracted one each other of value is > 0
+        .force("collide", d3.forceCollide().strength(.2).radius(function(d) {
+            return (size(d.value) + 3)
+        }).iterations(1)) // Force that avoids circle overlapping
+
+    // Apply these forces to the nodes and update their positions.
+    // Once the force algorithm is happy with positions ('alpha' value is low enough), simulations will stop.
+    simulation
+        .nodes(data)
+        .on("tick", function(d) {
+            node
+                .attr("cx", d => d.x)
+                .attr("cy", d => d.y)
+        });
+
+    // What happens when a circle is dragged?
+    function dragstarted(event, d) {
+        if (!event.active) simulation.alphaTarget(.03).restart();
+        d.fx = d.x;
+        d.fy = d.y;
+    }
+
+    function dragged(event, d) {
+        d.fx = event.x;
+        d.fy = event.y;
+    }
+
+    function dragended(event, d) {
+        if (!event.active) simulation.alphaTarget(.03);
+        d.fx = null;
+        d.fy = null;
+    }
+
+})
+
+// ----------------
+// GRAPH 4 - pie
+// ----------------
+
+// set the dimensions and margins of the graph
+const width_G4pie = 450,
+    height_G4pie = 450,
+    margin_G4pie = 40;
+
+// The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
+const radius = Math.min(width_G4pie, height_G4pie) / 2 - margin_G4pie;
+
+// append the svg object to the div called 'my_dataviz'
+const svg_G4pie = d3.select("#my_dataviz")
+    .append("svg")
+    .attr("width", width_G4pie)
+    .attr("height", height_G4pie)
+    .append("g")
+    .attr("transform", `translate(${width_G4pie/2}, ${height_G4pie/2})`);
+
+// Create data
+const data = {
+    a: 25.85,
+    b: 74.14
+}
+
+// set the color scale
+const color = d3.scaleOrdinal()
+    .range(["#004c9b", "#ffdc00"])
+
+// Compute the position of each group on the pie:
+const pie = d3.pie()
+    .value(function(d) {
+        return d[1]
+    })
+const data_ready = pie(Object.entries(data))
+
+// Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+svg_G4pie
+    .selectAll('whatever')
+    .data(data_ready)
+    .join('path')
+    .attr('d', d3.arc()
+        .innerRadius(0)
+        .outerRadius(radius)
+    )
+    .attr('fill', function(d) {
+        return (color(d.data[1]))
+    })
+    .attr("stroke", "black")
+    .style("stroke-width", "2px")
+    .style("opacity", 0.7)
